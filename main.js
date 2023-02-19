bb5636af4cb3d639faeafbefa64ea48df1ef6446
unknown <mitrovicbojan87@gmail.com>
2023-02-19 20:13:28 +0200

    final

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ for (let i = 0; i < inputs.length; i++) {
   let input = inputs[i];
 
   input.addEventListener("input", function (e) {
-    let value = parseFloat(e.target.value);
+    let value = Number(e.target.value);
 
     switch (e.target.name) {
       case "celsius":
-        fahrenheitInputs.value = ((value - 32) * 5) / 9;
+        fahrenheitInputs.value = value * 1.8 + 32;
         break;
       case "fahrenheit":
-        celsiusInputs.value = value * 1.8 + 32;
+        celsiusInputs.value = ((value - 32) * 5) / 9;
         break;
     }
   });
