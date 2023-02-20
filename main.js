956a136e3a0ed860fdff73a8da0a3c7c5bbf9774
unknown <mitrovicbojan87@gmail.com>
2023-02-20 20:41:51 +0200

    js

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 const celsiusInputs = document.getElementById("celsius");
-const fahrenheitInputs = document.getElementById("fahrenheit");
-const inputs = document.getElementsByClassName("input");
+const btn = document.getElementById("btn");
+const result = document.getElementById("calc");
 
-for (let i = 0; i < inputs.length; i++) {
-  let input = inputs[i];
-
-  input.addEventListener("input", function (e) {
-    let value = Number(e.target.value);
-
-    switch (e.target.name) {
-      case "celsius":
-        fahrenheitInputs.value = value * 1.8 + 32;
-        break;
-      case "fahrenheit":
-        celsiusInputs.value = ((value - 32) * 5) / 9;
-        break;
-    }
-  });
-}
+btn.addEventListener("click", function () {
+  return (result.innerHTML = (celsiusInputs.value * 9) / 5 + 32);
+});
